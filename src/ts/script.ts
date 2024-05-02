17f0c4203b3d0fbdb9f171c93fd4a4ba03afb539
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-05-02 01:15:48 -0600

    Adelanto final

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,64 +1,43 @@
-
-function playFuntion(id: HTMLMediaElement) {
-    return id.play()
+function playFunction(id: HTMLMediaElement) {
+  return id.play();
 }
 
+// PARA EL ELEMENTO H1
+const elementoPadre = document.querySelector(".nombre"); // Agregar el nodo 
+const nombreUsuario = document.createElement("h1"); // Tipo de elemento
+const textoh1 = document.createTextNode("Kevin"); // Seleccionar elemento padre 
 
+// Resultado = "Kevin"
+elementoPadre!.appendChild(nombreUsuario);
+nombreUsuario.appendChild(textoh1);
 
+// PARA EL ELEMENTO H2 
+const elementoPadre2 = document.querySelector(".apellido"); // Agregar el nodo 
+const apellidoUsuario = document.createElement("h1");
+const textoh2 = document.createTextNode("Alpizar"); // Seleccionar elemento padre 
 
-
- // PARA EL ELEMENTO H1
- const elementoPadre = document.querySelector(".nombre")//Agregar el nodo 
- const nombreUsuario = document.createElement("h1")//Tipo de elemento
- const textoh1 = document.createTextNode("Kevin")//Seleccionar elemento padre 
-
- 
- //  Result = "Kevin"
- elementoPadre!.appendChild(nombreUsuario)
- nombreUsuario.appendChild(textoh1)
- 
- 
- // PARA EL ELEMENTO H2 
- const elementoPadre2 = document.querySelector(".apellido")//Agregar el nodo 
- const apellidoUsuario = document.createElement("h1")
- const textoh2 = document.createTextNode("Alpizar")//Seleccionar elemento padre 
-  
-//  Result = "Alpizar"
- elementoPadre2!.appendChild(apellidoUsuario)
- apellidoUsuario.appendChild(textoh2)
-
-
-
-
-
-
-
+// Resultado = "Alpizar"
+elementoPadre2!.appendChild(apellidoUsuario);
+apellidoUsuario.appendChild(textoh2);
 
 // FUNCION PARA LA PRIMER BARRA Ruby On Rails
-
 var i = 0;
 
-function move(id: HTMLElement,porcent: number) {
+function move(id: HTMLElement, porcent: number) {
   if (i == 0) {
-    i = 1;
-   
-    var width = 0;
-    var identificador = setInterval(frame, 40);
-  
+      i = 1;
+      var width = 0;
+      var identificador = setInterval(frame, 40);
 
-    function frame() {
-      if (width >= porcent) {
-        clearInterval(identificador);
-        i = 0;
+      function frame() {
+          if (width >= porcent) {
+              clearInterval(identificador);
+              i = 0;
+          } else {
+              width++;
+              id!.style.width = width + "%";
+          }
       }
-       else {
-        width++;
-        id!.style.width = width + "%";
-    
-      }
-    }
-
-
   }
 }
-
+ 
